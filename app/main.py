6734c38442f6c607d7dc15ae34692ef486a533b9
alex <allexander0509@gmail.com>
2022-06-27 14:24:09 +0300

    Moved base.py to right place

--- app/main.py
+++ app/main.py
@@ -9,5 +9,10 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.on_event("startup")
+async def startup():
+    pass
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8081, host="0.0.0.0", reload=True)
