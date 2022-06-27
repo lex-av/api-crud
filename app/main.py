06e0332092dc79e2a5cb1a8bd7865cc38ff44677
alex <allexander0509@gmail.com>
2022-06-27 14:11:04 +0300

    Initialised app structure. Created entry point in main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8081, host="0.0.0.0", reload=True)
