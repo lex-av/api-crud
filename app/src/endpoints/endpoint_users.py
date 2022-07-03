165951aa38cf98ff052327433be0ce893bd07f2e
alex <allexander0509@gmail.com>
2022-07-03 21:44:40 +0300

    Finished read_users endpoint, added create_user endpoint

--- app/src/endpoints/endpoint_users.py
+++ app/src/endpoints/endpoint_users.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
+from src.models.model_user import User, UserInput
 from src.repositories.repo_user import UserRepository
 
 from .depends import get_user_repo
@@ -6,6 +9,11 @@ from .depends import get_user_repo
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_model=List[User])
 async def read_users(users: UserRepository = Depends(get_user_repo), limit: int = 100, skip: int = 1):
-    return {}
+    return await users.get_all(limit=limit, skip=skip)
+
+
+@router.post("/", response_model=User)
+async def create_user(user: UserInput, users: UserRepository = Depends(get_user_repo)):
+    return await users.create(user_in=user)
